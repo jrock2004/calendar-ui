acb3ae50d830d545e9a6ae98228f24f07e4f3941
John Costanzo <jrock2004@gmail.com>
2020-10-23 22:51:40 -0400

    Working on come coloring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import FullCalendar from '@fullcalendar/react'
 import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid';
 
+import resources from './data/resources';
 import './App.css';
 
 function App() {
-  const resources = [
-    { id: 1, title: 'John', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpickaface.net%2Fgallery%2Favatar%2Funr_sample_161118_2054_ynlrg.png&f=1&nofb=1'},
-    { id: 2, title: 'Matt', image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fdemos.microdesign-web.com%2Fsample_admin%2Fassets%2Fimg%2Favatar.jpg&f=1&nofb=1'},
-    { id: 3, title: 'Eddie', image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpickaface.net%2Fgallery%2Favatar%2FBenjohnsone54fbec7a167c5.png&f=1&nofb=1'}
-  ];
+  const businessHours = {
+    startTime: "08:00",
+    endTime: "17:00"
+  }
 
   const resourceContent = ({resource}) => {
     const { title, extendedProps} = resource;
@@ -40,6 +40,9 @@ function App() {
           allDaySlot={false}
           resources={resources}
           resourceLabelContent={resourceContent}
+          slotMinTime="08:00:00"
+          slotDuration="00:15:00"
+          nowIndicator={true}
         />
       </main>
     </>
---
+++ src/data/resources.js
@@ -0,0 +1,32 @@
+let resources = [
+  {
+    "id": 1,
+    "title": "John",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpickaface.net%2Fgallery%2Favatar%2Funr_sample_161118_2054_ynlrg.png&f=1&nofb=1",
+    "businessHours": {
+      startTime: '08:00',
+      endTime: '13:00'
+    },
+    "color": "red"
+  },
+  {
+    "id": 2,
+    "title": "Matt",
+    "image": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fdemos.microdesign-web.com%2Fsample_admin%2Fassets%2Fimg%2Favatar.jpg&f=1&nofb=1",
+    "businessHours": {
+      startTime: '08:00',
+      endTime: '14:00'
+    }
+  },
+  {
+    "id": 3,
+    "title": "Eddie",
+    "image": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpickaface.net%2Fgallery%2Favatar%2FBenjohnsone54fbec7a167c5.png&f=1&nofb=1",
+    "businessHours": {
+      startTime: '08:00',
+      endTime: '16:00'
+    }
+  }
+]
+
+export default resources;
