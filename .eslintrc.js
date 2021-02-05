7bf2bdcc360c3ebe010d500e979732ade13d9a6c
John Costanzo <jrock2004@gmail.com>
2021-02-05 09:31:11 -0500

    Adding babel eslint so I can use arrow function

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
     sourceType: 'module', // Allows using import/export statements
